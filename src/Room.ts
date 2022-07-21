af94589577cd1a234c2a3bfe9f2e87635db3409e
Snickbit <snickbit@users.noreply.github.com>
2022-07-21 02:22:35 -0400

    chore: remove legacy jsdocs

--- src/Room.ts
+++ src/Room.ts
@@ -4,15 +4,6 @@ export class Room {
 	width: number
 	height: number
 
-	/**
-   * @desc Helper class for drawing rooms when generating dungeons
-   * @constructor
-   *
-   * @param {Number} x - The x coordinate of the top side of the room
-   * @param {Number} y - The y coordinate of the left hand side of the room
-   * @param {Number} width - The width of the room
-   * @param {Number} height - The height of the room
-   */
 	constructor(x: number, y: number, width: number, height: number) {
 		this.x = x
 		this.y = y
@@ -20,13 +11,6 @@ export class Room {
 		this.height = height
 	}
 
-	/**
-	 * @desc Returns the bounding box for this room
-	 * @function
-	 *
-	 * @returns {Object} - Bounding box object containing a top, right, bottom and
-	 * left value.
-	 */
 	getBoundingBox() {
 		return {
 			top: this.y,
@@ -36,14 +20,6 @@ export class Room {
 		}
 	}
 
-	/**
-	 * @desc Compares this room with an entity that has a bounding box method to see
-	 * if they intersect.
-	 *
-	 * @param {Object} other - An object with a getBoundingBox() method
-	 *
-	 * @returns {Boolean} - true if there is an intersection
-	 */
 	intersects(other) {
 		if (!other.getBoundingBox) {
 			throw new Error('Given entity has no method getBoundingBox')
@@ -67,11 +43,6 @@ export class Room {
 		)
 	}
 
-	/**
-	 * @desc Returns a simple POJO representing this room
-	 *
-	 * @returns {Object} - A POJO
-	 */
 	toJson() {
 		return {
 			x: this.x,
