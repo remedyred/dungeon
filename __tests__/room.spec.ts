da9082c1e2149e401a723031ee7ced30eb7ad099
Snickbit <snickbit@users.noreply.github.com>
2022-07-21 02:23:53 -0400

    test: refactor room tests

--- __tests__/room.spec.ts
+++ __tests__/room.spec.ts
@@ -17,82 +17,85 @@ describe('Room', () => {
 		expect(new Room(2, 2, 10, 10).height).toBe(10)
 	})
 
-	it('Room.containsTile() returns false for tiles north of the room', () => {
-		const room = new Room(0, 0, 10, 10)
+	describe('Room.containsTile()', () => {
+		it('returns false for tiles north of the room', () => {
+			const room = new Room(0, 0, 10, 10)
 
-		expect(room.containsTile(0, -1)).toBe(false)
+			expect(room.containsTile(0, -1)).toBe(false)
+		})
+
+		it('returns false for tiles east of the room', () => {
+			const room = new Room(0, 0, 10, 10)
+
+			expect(room.containsTile(11, 0)).toBe(false)
+		})
+
+		it('returns false for tiles south of the room', () => {
+			const room = new Room(0, 0, 10, 10)
+
+			expect(room.containsTile(0, 11)).toBe(false)
+		})
+
+		it('returns false for tiles west of the room', () => {
+			const room = new Room(0, 0, 10, 10)
+
+			expect(room.containsTile(-1, 0)).toBe(false)
+		})
+
+		it('returns true for tiles inside of the room', () => {
+			const room = new Room(0, 0, 3, 3)
+
+			expect(room.containsTile(0, 2)).toBe(true)
+			expect(room.containsTile(1, 2)).toBe(true)
+			expect(room.containsTile(2, 2)).toBe(true)
+
+			expect(room.containsTile(0, 1)).toBe(true)
+			expect(room.containsTile(1, 1)).toBe(true)
+			expect(room.containsTile(2, 1)).toBe(true)
+
+			expect(room.containsTile(0, 0)).toBe(true)
+			expect(room.containsTile(1, 0)).toBe(true)
+			expect(room.containsTile(2, 0)).toBe(true)
+		})
 	})
 
-	it('Room.containsTile() returns false for tiles east of the room', () => {
-		const room = new Room(0, 0, 10, 10)
+	describe('Room.intersects()', () => {
+		it('Should throw if the object has no getBoundingBox method', () => {
+			expect(() => {
+				new Room(0, 0, 3, 3).intersects({foo: 'bar'})
+			}).toThrow()
+		})
 
-		expect(room.containsTile(11, 0)).toBe(false)
-	})
+		it('returns false for rooms north of the room', () => {
+			const room1 = new Room(0, 10, 10, 10)
+			const room2 = new Room(0, 0, 10, 10)
 
-	it('Room.containsTile() returns false for tiles south of the room', () => {
-		const room = new Room(0, 0, 10, 10)
+			expect(room1.intersects(room2)).toBe(false)
+		})
 
-		expect(room.containsTile(0, 11)).toBe(false)
-	})
+		it('returns false for rooms east of the room', () => {
+			const room1 = new Room(0, 0, 10, 10)
+			const room2 = new Room(10, 0, 10, 10)
 
-	it('Room.containsTile() returns false for tiles west of the room', () => {
-		const room = new Room(0, 0, 10, 10)
+			expect(room1.intersects(room2)).toBe(false)
+		})
 
-		expect(room.containsTile(-1, 0)).toBe(false)
-	})
+		it('returns false for rooms south of the room', () => {
+			const room1 = new Room(0, 0, 10, 10)
+			const room2 = new Room(0, 10, 10, 10)
 
-	it('Room.containsTile() returns true for tiles inside of the room', () => {
-		const room = new Room(0, 0, 3, 3)
+			expect(room1.intersects(room2)).toBe(false)
+		})
 
-		expect(room.containsTile(0, 2)).toBe(true)
-		expect(room.containsTile(1, 2)).toBe(true)
-		expect(room.containsTile(2, 2)).toBe(true)
+		it('returns false for rooms west of the room', () => {
+			const room1 = new Room(10, 0, 10, 10)
+			const room2 = new Room(0, 0, 10, 10)
 
-		expect(room.containsTile(0, 1)).toBe(true)
-		expect(room.containsTile(1, 1)).toBe(true)
-		expect(room.containsTile(2, 1)).toBe(true)
+			expect(room1.intersects(room2)).toBe(false)
+		})
 
-		expect(room.containsTile(0, 0)).toBe(true)
-		expect(room.containsTile(1, 0)).toBe(true)
-		expect(room.containsTile(2, 0)).toBe(true)
-	})
-
-	it('Room.intersects() Should throw if the object has no getBoundingBox method', () => {
-		expect(() => {
-			new Room(0, 0, 3, 3).intersects({foo: 'bar'})
-		}).toThrow()
-	})
-
-	it('Room.intersects() returns false for rooms north of the room', () => {
-		const room1 = new Room(0, 10, 10, 10)
-		const room2 = new Room(0, 0, 10, 10)
-
-		expect(room1.intersects(room2)).toBe(false)
-	})
-
-	it('Room.intersects() returns false for rooms east of the room', () => {
-		const room1 = new Room(0, 0, 10, 10)
-		const room2 = new Room(10, 0, 10, 10)
-
-		expect(room1.intersects(room2)).toBe(false)
-	})
-
-	it('Room.intersects() returns false for rooms south of the room', () => {
-		const room1 = new Room(0, 0, 10, 10)
-		const room2 = new Room(0, 10, 10, 10)
-
-		expect(room1.intersects(room2)).toBe(false)
-	})
-
-	it('Room.intersects() returns false for rooms west of the room', () => {
-		const room1 = new Room(10, 0, 10, 10)
-		const room2 = new Room(0, 0, 10, 10)
-
-		expect(room1.intersects(room2)).toBe(false)
-	})
-
-	it('Room.intersects() returns true for rooms that intersect', () => {
-		const room = new Room(10, 10, 10, 10)
+		it('returns true for rooms that intersect', () => {
+			const room = new Room(10, 10, 10, 10)
 
 			expect(room.intersects(new Room(1, 1, 10, 10))).toBe(true)
 			expect(room.intersects(new Room(9, 9, 10, 10))).toBe(true)
